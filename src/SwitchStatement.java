efd2efea5e091656ce3da9b4eaaa334a2eb50e3e
Nur Faidzin <nurfaidzin@student.janabadra.ac.id>
2025-11-09 21:10:13 +0700

    Swicth Statement

--- src/SwitchStatement.java
+++ src/SwitchStatement.java
@@ -3,7 +3,19 @@ public class SwitchStatement {
         var score = "A";
 
         switch (score) {
-            
+            case "A":
+                System.out.println("Wow, you did great!");
+                break;
+            case "B":
+            case "C":
+                System.out.println("Well done, keep trying!");
+                break;
+            case "D":
+                System.out.println("You passed, but there's room for improvement.");
+                break;
+            default:
+                System.out.println("Maybe you chose the wrong major?");
+                break;
         }
     }
 }
